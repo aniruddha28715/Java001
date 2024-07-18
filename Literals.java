742c28b2b759cae4e51485bf85d757216e9867e5
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-18 21:14:34 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,21 @@
+public class Literals {
+    public static void main(String[] args){
+        //write your code here
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 33D;
+        boolean b1 = true;
+        short as = 3432;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(b1);
+        System.out.println(as);
+        String str = "Ani";
+        System.out.println(str);
+        String ani = "str";
+        System.out.println(ani);
+    }
+}
