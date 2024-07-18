61a6288a9606cb4638d8d77c4664da8b0231bf32
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-18 21:57:03 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators{
+    public static void main(String[] args){
+        //write your code here
+        int a = 3;
+        int b = a * 4;
+
+        int b1 = 3;
+        b1 *= 23;
+        System.out.println(b1);
+        System.out.println(b);
+        System.out.println(64>5 && 64>7);
+        System.out.println(64>100 || 64>99);
+
+    }
+}
