2ee91aa5b550f30a540bb5d74d5dbb59403ce9b0
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-18 22:17:31 +0530

    Add files via upload

---
+++ IncreDecreOperator.java
@@ -0,0 +1,21 @@
+public class IncreDecreOperator {
+    public static void main(String[] args) {
+        int a = 7;
+        
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+
+        int x = ++a*8;
+        System.out.println(x);
+
+        char g = 'b';
+        System.out.println(g++);
+
+
+}
+}
+
+
