120ba28cae559022589b92e05d889435de44e88b
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-18 20:50:10 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args){
+        //write your code here
+        System.out.print("The sum is: ");
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+    }
+}
+
