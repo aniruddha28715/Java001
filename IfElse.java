ee200769a758084008f7948ae151e2f9b0bbc940
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-19 18:42:29 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 28;
+        boolean cond = (age==18);
+        if (cond) {
+            System.out.println("You are eligible to vote");
+        }
+        else {
+            System.out.println("You are not eligible to vote");
+        }
+}
+}
