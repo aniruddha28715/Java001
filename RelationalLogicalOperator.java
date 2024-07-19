130a9cdef05b76fa275c86e5f58140a1ca3a3811
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-19 18:48:22 +0530

    Add files via upload

---
+++ RelationalLogicalOperator.java
@@ -0,0 +1,13 @@
+public class RelationalLogicalOperator {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
+        if(a && b && c){
+            System.out.println("a and b is true");
+        }
+        else{
+            System.out.println("a and b is false");
+        }
+    }
+}
