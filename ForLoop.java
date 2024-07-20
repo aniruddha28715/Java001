d4af81c6a62d68ed56b4f95a808328dc6eab6246
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-20 12:54:27 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,22 @@
+public class ForLoop{
+    public static void main(String[] args) {
+        int a = 1;
+    //     for (int i = 0; i < 10; i++) {
+    //         System.out.println(a);
+            
+    // }
+    //2n = Even numbers = 0,2,4,6,8,..
+    //2n+1 = Odd numbers = 1,3,5,7,9...
+    // int n = 3;
+    // for(int i=0; i<n; i++){
+    //     System.out.println(2*i+1);
+    // }
+
+    for(int i=5; i>0 ;i--){   //decrementing for loop
+        System.out.println(i);
+
+    }
+    
+
+}
+}
