bb5ff0bc28014cd875aa61c678fc128b5a5d9d39
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-20 12:13:57 +0530

    Add files via upload

---
+++ WhileLoops.java
@@ -0,0 +1,13 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("End of loop");
+        while(true)
+        System.out.println("Infinity while loop");
+    }
+    
+}
