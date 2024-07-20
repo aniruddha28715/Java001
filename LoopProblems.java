e74b813bde5802ebd386c8f476fec2673399264c
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-20 18:35:46 +0530

    Add files via upload

---
+++ LoopProblems.java
@@ -0,0 +1,47 @@
+public class LoopProblems {
+    public static void main(String[] args) {
+        //Problem 1
+        // int n = 4;
+        // for(int i = n; i>0; i--){
+        //     for(int j = 0; j<i; j++){
+        //         System.out.print("*");
+                
+        //     }
+            
+        //     System.out.println("\n"); 
+        // }
+
+        //Problem 2
+
+        // int sum  = 0;
+        // int n = 4;
+        // for(int i= 0; i<n ; i++){
+        //     sum = sum + (2*i);
+
+
+        // }
+        // System.out.print("Sum of even number si: ");
+        // System.out.println(sum); 
+
+
+        //Problem 3
+
+        // int n = 6;
+        // for(int i = 0; i<10; i++){
+        //     System.out.printf("%d X %d = %d\n", n, i, n*i);
+        // }
+
+
+        
+
+
+
+
+
+
+
+
+
+    
+}
+}
