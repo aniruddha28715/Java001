915c5d3ccf95c7476ab4c98582af1e8279997001
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-20 12:25:18 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,23 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        // while(i<=5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<45);
+
+        int c = 1;
+        do{
+            System.out.println(c);
+
+        }while(c<=45);
+
+    
+    }
+}
