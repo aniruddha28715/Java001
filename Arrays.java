e2c3508c48e0c19c3dd87233c19fdf7db19c3df8
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-21 14:11:28 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // int[] marks = new int[5];
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+        // marks[3] = 40;
+        // marks[4] = 50;
+        // System.out.println(marks[0]);
+
+
+        // int[] marks;
+        // marks = new int[5];
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+        // marks[3] = 40;
+        // marks[4] = 50;
+        // System.out.println(marks[0]);
+
+
+        int[] marks = {20,45,23,55,43};
+        System.out.println(marks[0]);
+
+
+    }
+}
