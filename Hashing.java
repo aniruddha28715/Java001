b7c451f30c9a933e2e67430cd42f209084b0d18f
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-07-30 21:38:35 +0530

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Hashing {
+    public static int union(int arr1[], int arr2[]){
+        HashSet<Interger> set = new HashSet<>();
+
+
+        for(int i=0; i<arr1.length; i++){
+            ser.add(arr1[i]);
+
+
+        }
+        for(int j=0; j<arr2.length; j++){
+            ser.add(arr2[j]);
+
+
+        }
+
+        return set.size();
+
+
+    }
+    public static void main(String[] args) {
+        int arr1[] = {1, 2, 3, 4, 5};
+        int arr2[] = {4, 5, 6, 7, 8, 6, 4};
+    }
+}
